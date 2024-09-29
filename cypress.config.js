19b52f766c6feb8a4909e6b64085fc9eadda1923
AdoDeveloper <ayudacuenta541@gmail.com>
2024-09-29 03:26:41 -0600

    Ajustes en el yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
 // cypress.config.js
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents() {
       // Implementar cualquier evento o plugin si es necesario
@@ -7,10 +9,17 @@ module.exports = {
     baseUrl: 'http://localhost:3000',
     supportFile: 'cypress/support/index.js',
     specPattern: 'cypress/integration/**/*.spec.js',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+    },
   },
   video: true,
   screenshotsFolder: "cypress/screenshots",
   videosFolder: "cypress/videos",
   downloadsFolder: "cypress/downloads",
   fixturesFolder: "cypress/fixtures"
-};
+});
